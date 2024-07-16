5a211fcfc3ebb33002dbd3b7a4fac3faf37b4d11
orlando1986 <310849314@qq.com>
2024-07-17 00:04:41 +0800

    fix building bug
    Signed-off-by: orlando1986 <310849314@qq.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation("androidx.camera:camera-extensions:$camerax_version")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
